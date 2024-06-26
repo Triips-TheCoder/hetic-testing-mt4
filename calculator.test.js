c900d3b678dd70249a8d0f1d56911de755eed0ba
Julian Laballe <julian.laballe@unlikely.technology>
2024-06-26 16:05:38 +0200

    feat: add substract test

--- calculator.test.js
+++ calculator.test.js
@@ -4,4 +4,7 @@ test('adds 1 + 2 to equal 3', () => {
     expect(rpnCalculator(1, 2, '+')).toBe(3);
 });
 
+test('subtracts 5 - 3 to equal 2', () => {
+    expect(rpnCalculator(5, 3, '-')).toBe(2);
+});
 
