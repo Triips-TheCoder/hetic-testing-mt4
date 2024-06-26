b68e11ecd99e7ca6e8fcc104fb804b99746bdc4d
Julian Laballe <julian.laballe@unlikely.technology>
2024-06-26 15:48:57 +0200

    feat: add a second test for subtract function

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,9 @@
-const sum = require('./calculator');
+const { sum, subtract } = require('./calculator');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test('subtracts 5 - 2 to equal 3', () => {
+    expect(subtract(5, 2)).toBe(3);
 });
