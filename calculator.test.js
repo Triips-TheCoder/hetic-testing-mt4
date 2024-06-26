b84fc2c807110775979323bbd33cd0504b2bb431
Julian Laballe <julian.laballe@unlikely.technology>
2024-06-26 16:06:13 +0200

    feat: add multiplies test

--- calculator.test.js
+++ calculator.test.js
@@ -8,3 +8,7 @@ test('subtracts 5 - 3 to equal 2', () => {
     expect(rpnCalculator(5, 3, '-')).toBe(2);
 });
 
+test('multiplies 4 * 2 to equal 8', () => {
+    expect(rpnCalculator(4, 2, '*')).toBe(8);
+});
+
