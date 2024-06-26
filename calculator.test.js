2f38390c8e6d1b4db9a54c26a4d01d85a763cb55
Julian Laballe <julian.laballe@unlikely.technology>
2024-06-26 16:07:17 +0200

    feat: add throw error test

--- calculator.test.js
+++ calculator.test.js
@@ -12,3 +12,14 @@ test('multiplies 4 * 2 to equal 8', () => {
     expect(rpnCalculator(4, 2, '*')).toBe(8);
 });
 
+test('throws error for invalid operator', () => {
+    expect(() => rpnCalculator(4, 2, '/')).toThrow('Invalid operator');
+});
+
+test('throws error when num1 is not a number', () => {
+    expect(() => rpnCalculator('a', 2, '+')).toThrow('num1 and num2 must be numbers');
+});
+
+test('throws error when num2 is not a number', () => {
+    expect(() => rpnCalculator(4, 'b', '+')).toThrow('num1 and num2 must be numbers');
+});
