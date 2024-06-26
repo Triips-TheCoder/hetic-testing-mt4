812bbd56b2df3056df82ae283e9e996ccf0fb312
Julian Laballe <julian.laballe@unlikely.technology>
2024-06-26 15:57:14 +0200

    feat: add new function to calculator js

--- calculator.js
+++ calculator.js
@@ -10,4 +10,33 @@ function multiply(a, b) {
     return a * b;
 }
 
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Division par zéro");
+    }
+    return a / b;
+}
+
+function modulus(a, b) {
+    return a % b;
+}
+
+function power(a, b) {
+    return Math.pow(a, b);
+}
+
+function squareRoot(a) {
+    if (a < 0) {
+        throw new Error("Racine carrée d'un nombre négatif");
+    }
+    return Math.sqrt(a);
+}
+
+function absolute(a) {
+    return Math.abs(a);
+}
+
+
+
+
 module.exports = { sum, subtract, multiply };
